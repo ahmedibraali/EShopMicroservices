e94fbecfcaaae8af9cdaed39e24dc2740a266f84
Ahmad Ibrahim <50780343+ahmedibraali@users.noreply.github.com>
2024-05-23 18:01:39 +0300

    Handling creating product feature over posgresql and containerizing this feature on Docker

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductCommandHandler(IDocumentSession session) 
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            // Business Logic to create product
+            ///create Product entity from command object
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+            ///save to database 
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);///TODO
+
+
+            ///return CreateProductResult result  
+            return new CreateProductResult(product.Id);
+
+            throw new NotImplementedException();
+        }
+    }
+}
